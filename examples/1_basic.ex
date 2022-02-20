5e66ddf48ff935c22d9ce3fdc839ff03f7f83090
odelbos <od@phibox.com>
2022-02-20 22:48:15 +0100

    feat: add error handling
    now the 'final' function will return
    {:ok, blocks}
    or
    {:error, reason, msg, blocks}

--- examples/1_basic.ex
+++ examples/1_basic.ex
@@ -12,18 +12,58 @@ lorem_filepath = Path.join [current_path, "test", "data", "lorem.txt"]
 # Split in blocks of 512 bytes
 size = 512
 storage_path = Path.join [current_path, "storage", "blocks"]
+# storage_path = Path.join [current_path, "storage", "not-exists"]
 
-{:ok, blocks} = %CryptoBlocks{storage: storage_path, size: size}
+# {:ok, blocks} = %CryptoBlocks{storage: storage_path, size: size}
+#   |> CryptoBlocks.write(data)
+#   |> CryptoBlocks.final()
+
+result = %CryptoBlocks{storage: storage_path, size: size}
   |> CryptoBlocks.write(data)
   |> CryptoBlocks.final()
 
-IO.inspect blocks
+case result do
+  {:error, reason, blocks} ->
+    IO.puts "We receive an error : #{IO.inspect reason}"
+    IO.puts "blocks:"
+    IO.inspect blocks
+    System.halt 0
+  _ -> result
+end
+
+{:ok, blocks} = result
+
+# IO.inspect blocks
 
 # -----
 
+# Generate an error by corrupting a block
+# c_block = Enum.at blocks, 3
+# IO.inspect c_block
+# corrupt_filepath = Path.join [storage_path, CryptoBlocks.id_to_name(c_block.id)]
+# File.write corrupt_filepath, :crypto.strong_rand_bytes(512)
+# File.close corrupt_filepath
+
+# -----
+
+# error_path = Path.join [current_path, "storage", "not-exists"]
+
 # Rebuild the file
 dest = Path.join [current_path, "storage", "rebuild_lorem.txt"]
-CryptoBlocks.rebuild blocks, storage_path, dest
+# dest = Path.join [current_path, "error", "rebuild_lorem.txt"]
+
+case CryptoBlocks.rebuild blocks, storage_path, dest do
+  :ok ->
+    IO.puts "File rebuilded"
+  {:error, reason, msg} ->
+    IO.puts "===> We got an error : #{IO.inspect reason}"
+    IO.puts "===> Message         : #{IO.inspect msg}"
+    System.halt 0
+  e ->
+    IO.inspect e
+    IO.puts "===> We got an unknow error"
+    System.halt 0
+end
 
 # -----
 
--- lib/crypto_blocks.ex
+++ lib/crypto_blocks.ex
@@ -22,9 +22,23 @@ defmodule CryptoBlocks do
 
   def write(%CryptoBlocks{} = struct, data) when struct.acc == nil do
     make_blocks struct, data
+  rescue
+    # %File.Error{reason: reason} -> IO.inspect reason
+    e -> case e do
+           %File.Error{reason: reason} ->
+             {:error, reason, struct}
+           %ErlangError{original: {reason, _, _}} ->
+             {:error, reason, struct}
+           _ ->
+             {:error, :unknow, struct}
+         end
   end
 
   # -----
+
+  def final({:error, reason, struct}) when struct.acc == nil do
+    {:error, reason, Enum.reverse struct.blocks}
+  end
 
   def final(%CryptoBlocks{} = struct) when struct.acc == nil do
     {:ok, Enum.reverse struct.blocks}
@@ -62,10 +76,10 @@ defmodule CryptoBlocks do
   # -----
 
   def write_block(%CryptoBlocks{} = struct, data) do
-    {key, iv, tag, encrypted} = Crypto.encrypt data             # TODO : error handling
+    {key, iv, tag, encrypted} = Crypto.encrypt data         # TODO : error handling
     id = generate_id struct.storage
     dest = Path.join [struct.storage, id_to_name(id)]
-    File.write dest, encrypted, [:binary, :raw]                 # TODO : error handling
+    File.write! dest, encrypted, [:binary, :raw]            # TODO : error handling
     %{struct | blocks: [%{id: id, key: key, iv: iv, tag: tag} | struct.blocks]}
   end
 
@@ -86,8 +100,20 @@ defmodule CryptoBlocks do
   # -----
 
   def rebuild(blocks, storage, filepath) do
-    {:ok, file} = File.open filepath, [:write, :binary, :raw]   # TODO : error handling
+    file = File.open! filepath, [:write, :binary, :raw]
     do_rebuild blocks, storage, file
+  rescue
+    e -> case e do
+           {:error, reason} ->
+             {:error, reason,"File system error 1"}
+           %File.Error{reason: reason} ->
+             {:error, reason, "File system error 2"}
+           %ErlangError{original: {reason, _, _}} ->
+             {:error, reason, "Cannot decrypt a block"}
+           _ ->
+             IO.inspect e
+             {:error, :unknow, "Unknow error"}
+         end
   end
 
   defp do_rebuild([], _storage, file) do
@@ -96,9 +122,13 @@ defmodule CryptoBlocks do
   end
 
   defp do_rebuild([block | tail], storage, file) do
-    data = read_block block, storage
-    IO.binwrite file, data                                      # TODO : error handling
-    do_rebuild tail, storage, file
+    case read_block block, storage do
+      :error ->
+        {:error, :decrypt, "Block corrupted"}
+      data ->
+        IO.binwrite file, data
+        do_rebuild tail, storage, file
+    end
   end
 
   # -----
@@ -106,8 +136,8 @@ defmodule CryptoBlocks do
   def read_block(block, storage) do
     filename = id_to_name block.id
     filepath = Path.join [storage, filename]
-    {:ok, encrypted} = File.read filepath                       # TODO : error handling
-    Crypto.decrypt encrypted, block.key, block.iv, block.tag    # TODO : error handling
+    encrypted = File.read! filepath
+    Crypto.decrypt encrypted, block.key, block.iv, block.tag
   end
 
   # -----
